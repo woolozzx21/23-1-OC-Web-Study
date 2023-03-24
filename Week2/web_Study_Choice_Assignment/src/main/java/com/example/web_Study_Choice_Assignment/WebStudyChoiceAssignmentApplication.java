416fdba54c62ca2cc5c8378594b0aaff38abb468
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 <80960745+woolozzx21@users.noreply.github.com>
2023-03-24 19:21:08 +0900

    Spring 프로젝트 커밋

---
+++ Week2/web_Study_Choice_Assignment/src/main/java/com/example/web_Study_Choice_Assignment/WebStudyChoiceAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_Study_Choice_Assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStudyChoiceAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebStudyChoiceAssignmentApplication.class, args);
+	}
+
+}
---
+++ Week2/web_Study_Choice_Assignment/src/test/java/com/example/web_Study_Choice_Assignment/WebStudyChoiceAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_Study_Choice_Assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebStudyChoiceAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
